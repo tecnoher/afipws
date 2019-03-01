cf9ae6b2936ff148a7f134e83e91e1a00b6c6a46
Hernan T <tecnoher@gmail.com>
2019-03-01 01:48:12 -0300

    First commit

---
+++ afip/CHANGELOG.md
@@ -0,0 +1,12 @@
+# API-AFIP
+
+## CHANGELOG
+
+[CURRENT](/../compare/master...develop)
+
+
+[v1.0.1](/../tree/v1.0.1) - 2018-11-08
+* Se cambia el link a la documentación de afip
+
+[v1.0.0](/../tree/v1.0.0) - 2018-09-18
+* Release inicial
---
+++ afip/src/Afip/Error/WebServiceNotImplementedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AfipWS\Afip\Error;
+
+class WebServiceNotImplementedException extends CustomException
+{
+    public function __construct($title, $message, $code = 550, \Exception $previous = null)
+    {
+        parent::__construct($title, $message, $code, $previous);
+    }
+}
